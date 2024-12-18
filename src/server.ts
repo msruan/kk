5c1b3ef95ab30d6bfd22b41243c5dafbadce3c3c
Ruan <santosruan2021@gmail.com>
2024-12-18 18:56:07 -0300

    feat: change server to express

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
-import { fastify } from "fastify";
+import express from "express";
+import "express-async-errors";
 
-const app = fastify({ logger: true });
+const app = express();
 
 app.get("/", (req, res) => {
-  return { hello: "Hi!!!" };
+  res.json({ hello: "Hi!!!" });
 });
 
-app.listen({ port: 3000 });
+app.listen({ port: 3000 }, () => {
+  console.log("Express listening on port 3000!");
+});
